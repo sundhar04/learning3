27aef5a0af4d671a3fb138720318508ab0cab219
Sundhar <167981588+sundhar04@users.noreply.github.com>
2025-07-22 12:22:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-a=20
-b=20
-c=5
-print(a+b+c)
+from flask import Flask
+app= Flask(__name__)
+@app.route('/',methods=['GET'])
+def summ():
+    a=20
+    b=30
+    c=-5
+    sum=a+b+c
+    return f"the total is{sum}"
+
+
+if __name__ == "__main__":
+    
+    app.run(host="0.0.0.0",port=5000)
+    
+#docker build -t yourusername/yourimage:latest .
+#docker push yourusername/yourimage:latest
